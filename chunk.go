84de70be09a62ab428c77935f441d3ed68cf4655
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-05-30 07:15:30 -0400

    Add msgpack tags

--- chunk.go
+++ chunk.go
@@ -1,10 +1,10 @@
 package network
 
 type Chunk struct {
-	ID   uint64
-	Seq  uint64
-	End  bool
-	Data []byte
+	ID   uint64 `msgpack:"id"`
+	Seq  uint64 `msgpack:"seq"`
+	End  bool   `msgpack:"end"`
+	Data []byte `msgpack:"data"`
 }
 
 type WritePacketCallback func(w WritePacket, err error)
